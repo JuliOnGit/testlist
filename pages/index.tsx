16466a8f5b7e7b0cfc6ab28260e23aad0005ae77
Hohenöcker, Julian <Julian.Hohenoecker@adesso.de>
2023-04-28 12:04:21 +0200

    Implement parameter page and link it in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import ParameterView from '@/components/parameter-view';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main>
       <h1>Here is the ParameterView</h1>
-      <ParameterView></ParameterView>
+      <Link href={'/parameters-page'}>Go to parameter view</Link>
     </main>
   );
 }
---
+++ pages/parameters-page.tsx
@@ -0,0 +1,5 @@
+import ParameterView from '@/components/parameter-view';
+
+export default function ParametersPage() {
+  return <ParameterView></ParameterView>;
+}
