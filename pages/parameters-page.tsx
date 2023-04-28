b67cd35d63f8903f80570e3a082743cabda9b6cd
Hohenöcker, Julian <Julian.Hohenoecker@adesso.de>
2023-04-28 12:13:13 +0200

    Replace parameter-view by -page

--- pages/parameters-page.tsx
+++ pages/parameters-page.tsx
@@ -1,5 +1,28 @@
-import ParameterView from '@/components/parameter-view';
+import ParameterList from '@/components/parameter-list';
+import { Parameter } from '@/interfaces/parameter';
+import { useState } from 'react';
 
 export default function ParametersPage() {
-  return <ParameterView></ParameterView>;
+  const [parameterList, setParameterList] = useState<Parameter[]>([
+    {
+      id: 1,
+      name: 'first param',
+      defaultValue: 'a default value',
+      actualValue: 'another value'
+    },
+    {
+      id: 2,
+      name: 'next parameter',
+      defaultValue: 'a value',
+      actualValue: 'the same value'
+    },
+    {
+      id: 3,
+      name: 'third parameter',
+      defaultValue: 'another value',
+      actualValue: 'a different value'
+    }
+  ]);
+
+  return <ParameterList parameterList={parameterList}></ParameterList>;
 }
