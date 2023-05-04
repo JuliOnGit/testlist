e1c7138478795d4407ce6943f63eebedf2e789e6
Hohenöcker, Julian <Julian.Hohenoecker@adesso.de>
2023-05-04 11:26:47 +0200

    Render html table

--- components/parameter-list.tsx
+++ components/parameter-list.tsx
@@ -2,8 +2,27 @@ import { Parameter } from '@/interfaces/parameter';
 
 export default function ParameterList({ parameterList }: { parameterList: Parameter[] }) {
   return (
-    <>
-      {parameterList.map((p) => <li key={p.id}>{p.name}</li>)}
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Parameter Name</th>
+          <th>Default Value</th>
+          <th>Actual Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {parameterList.map((p) =>
+          <tr key={p.id}>
+            <td>{p.id}</td>
+            <td>{p.name}</td>
+            <td>{p.defaultValue}</td>
+            <td>{p.actualValue}</td>
+          </tr>)}
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
