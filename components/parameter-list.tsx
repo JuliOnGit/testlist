02eff734355add4200bc909ae5de247c9ce85e00
Hohenöcker, Julian <Julian.Hohenoecker@adesso.de>
2023-05-11 09:19:54 +0200

    Set table column widths

--- components/parameter-list.tsx
+++ components/parameter-list.tsx
@@ -6,10 +6,10 @@ export default function ParameterList({ parameterList }: { parameterList: Parame
     <table className='border-collapse table-auto w-full text-sm'>
       <thead>
         <tr>
-          <th className='border-b dark:border-slate-600 font-medium p-4 text-slate-400 dark:text-slate-200 text-left'>ID</th>
-          <th className='border-b dark:border-slate-600 font-medium p-4 text-slate-400 dark:text-slate-200 text-left'>Parameter Name</th>
-          <th className='border-b dark:border-slate-600 font-medium p-4 text-slate-400 dark:text-slate-200 text-left'>Default Value</th>
-          <th className='border-b dark:border-slate-600 font-medium p-4 text-slate-400 dark:text-slate-200 text-left'>Actual Value</th>
+          <th className='border-b dark:border-slate-600 font-medium p-4 text-slate-400 dark:text-slate-200 text-left w-1/12'>ID</th>
+          <th className='border-b dark:border-slate-600 font-medium p-4 text-slate-400 dark:text-slate-200 text-left w-5/12'>Parameter Name</th>
+          <th className='border-b dark:border-slate-600 font-medium p-4 text-slate-400 dark:text-slate-200 text-left w-3/12'>Default Value</th>
+          <th className='border-b dark:border-slate-600 font-medium p-4 text-slate-400 dark:text-slate-200 text-left w-3/12'>Actual Value</th>
         </tr>
       </thead>
       <tbody className='bg-white dark:bg-slate-800'>
